bb530b2968437bba14f19bf0b387c15b92ad69e1
Umitbeluly Aibarys <umitbekulyajbarys@gmail.com>
2024-10-30 16:48:58 +0500

    3 task

---
+++ Class Work/index3.js
@@ -0,0 +1,8 @@
+let surname = "UMITBEKULY"
+let relatives = ["Asigul", "umitbeluly", "Dastan", "and me Aibarys",];
+let relativesNumbeer = "Four person"
+
+
+console.log("Familiam", surname);
+console.log("OTBASYMNIN ATTARU", relatives);
+console.log("OTBASIMNIN SANY", relativesNumbeer);
