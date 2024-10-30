325f01781f6668e714c831853053765651930da9
Umitbeluly Aibarys <umitbekulyajbarys@gmail.com>
2024-10-30 16:36:09 +0500

    Exercise

---
+++ Class Work/index2.js
@@ -0,0 +1,20 @@
+
+let notebookName = "Isus Gaming A15"; 
+
+let processor = "Ryxen 7000series 5"; 
+
+let size = "16.4 duim"; 
+
+let color= "black"; 
+
+let todayWeather = "cloudy"; 
+
+let currentTime = "16:30"; 
+
+console.log("NOTEBOOKIM",notebookName );
+console.log("PROSSERIRM", processor);
+console.log("Olzhemi",size );
+console.log("Tysi",color );
+console.log("AYA RAIY",todayWeather );
+console.log("YAKIT",currentTime );
+
