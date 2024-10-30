83da5edbd0eaa2fa908bdd3ae227dda3800fbff8
Umitbeluly Aibarys <umitbekulyajbarys@gmail.com>
2024-10-30 16:22:34 +0500

    scrift

---
+++ Class Work/scriptt.js
@@ -0,0 +1,14 @@
+let name = "Aibarys";
+let surname = "Umitbekuly";
+const birthday = "01.08.2010";
+let school = "126 school lyceum";
+let weight = '57kg';
+
+console.log("BUL MENin Atym", name);
+console.log("BUL MENIN Familyam", surname);
+console.log("BUL MENIN TYGAN KYNIM", birthday);
+console.log("BUL MENIN MEKTEBIM", school);
+console.log("BUL MENIN SALMAGHYM", weight);
+
+
+
